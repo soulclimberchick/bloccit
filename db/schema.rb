32cf12d5528e4d588763be02d24c6e38b7ff8e1b
Kira Helm <soulclimberchick@gmail.com>
2018-08-04 18:36:01 -0700

    assignment 19 more crud completed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_23_041102) do
+ActiveRecord::Schema.define(version: 2018_08_04_220247) do
+
+  create_table "advertisements", force: :cascade do |t|
+    t.string "title"
+    t.text "copy"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "answers", force: :cascade do |t|
     t.text "body"
@@ -41,6 +49,14 @@ ActiveRecord::Schema.define(version: 2018_07_23_041102) do
     t.string "generate"
     t.string "model"
     t.string "Post"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.boolean "resolved"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,11 +1,10 @@
 require 'rails_helper'
+ RSpec.describe Question, type: :model do
+   let(:question) { Question.create!(title: "New Question Title", body: "New Question Body", resolved: false) }
 
-RSpec.describe Question, type: :model do
-  let (:question ) { Question.create!(title: "New Question Title", body: "New Question Body", resolved: true) }
-
-  describe "attributes" do
-    it "has title, body and resolved attributes" do
-      expect(question).to have_attributes(title: "New Question Title", body: "New Question Body", resolved: true)
-    end
-  end
+   describe "attributes" do
+     it "has title and body attributes" do
+       expect(question).to have_attributes(title: "New Question Title", body: "New Question Body", resolved:false)
+     end
+   end
 end
