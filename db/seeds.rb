79544b23123cb16ef6210b3f706dc7755d54da01
Kira Helm <soulclimberchick@gmail.com>
2018-08-08 16:58:56 -0700

    added unique comments in seeds.db

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,6 +20,9 @@ end
 Post.find_or_create_by(title: “Many Title“, body: “Voluminous Body”)
 Post.find_or_create_by(title: “So Titles, WOW!“, body: “Much Body”)
 Post.find_or_create_by(title: “Please call me by my title“, body: “This be the body”)
+Comment.find_or_create_by(post: "Unique Texty Things!", body: "Unique Body Things!")
+Comment.find_or_create_by(post: "Hey!", body: "Sup!")
+Comment.find_or_create_by(post: "More Post!", body: "More Body!")
 
 puts "Seed finished"
 puts "#{Post.count} posts created"
