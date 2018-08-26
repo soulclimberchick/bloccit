811de3b024748777a3a890b1e2d3fb14bf0a0fdf
Kira Helm <soulclimberchick@gmail.com>
2018-08-25 20:25:30 -0700

    Assignment 22 Validations completed

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,9 +1,6 @@
-class Topic < ApplicationRecord
+class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy
-  has_many :sponsoredposts
 
-  validates :title, length: { minimum: 5 }, presence: true
   validates :name, length: { minimum: 5 }, presence: true
   validates :description, length: { minimum: 15 }, presence: true
-  validates :topic, presence: true
 end
