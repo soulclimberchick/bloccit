9a27a1f94b718c2e322eef7bcdd3fb670a5beb5f
Kira Helm <soulclimberchick@gmail.com>
2018-10-07 16:03:01 -0700

    Accidentally committed before finising last previously. Checkpoint 24 signing up completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,12 +6,15 @@ class UsersController < ApplicationController
   def create
     @user = User.new
     @user.name = params[:user][:name]
-    @user.email = params[:user][:name]
+    @user.email = params[:user][:email]
     @user.password = params[:user][:password]
     @user.password_confirmation = params[:user][:password_confirmation]
 
     if @user.save
       flash[:notice] = "Welcome to Bloccit #{@user.name}!"
+      redirect_to root_path
+    else
+      flash.now[:alert] = "There was an error creating your account. Please try again."
       render :new
     end
   end
