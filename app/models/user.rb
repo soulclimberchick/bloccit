f722de4c0c720251adaf171661b81309679beafb
Kira Helm <soulclimberchick@gmail.com>
2018-10-15 19:23:01 -0700

    Checkpoint 27 Authorization completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,19 @@
 class User < ApplicationRecord
-  has_many :posts, dependent: :destroy
+  has_many :posts
 
-  before_save { self.email = email.downcase if email.present? }
+  before_save { self.email = email.try (:downcase) }
+  before_save { self.role ||= :member }
 
-  validates :name,
-            length: { minimum: 1, maximum: 100 },
-            presence: true
-  validates :password, presence: true, length: { minimum: 6 }, if: -> { password_digest.nil? }
-  validates :password, length: { minimum: 6 }, allow_blank: true
+  validates :name, length: { minimum: 1, maximum: 100 }, presence: true
+
   validates :email,
             presence: true,
             uniqueness: { case_sensitive: true },
             length: { minimum: 3, maximum: 254 }
+  validates :password, presence: true, length: { minimum: 6 }, if: -> { "password_digest.nil?" }
+  validates :password, length: { minimum: 6 }, allow_blank: true
+
   has_secure_password
 
+  enum role: [:member, :admin]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -38,12 +38,18 @@ end
          )
 end
 
-user = User.first
-user.update_attributes!(
-  email: 'soulclimberchick@gmail.com',
-  password: 'helloworld'
+admin = User.create!(
+  name:     'Admin User',
+  email:    'admin@example.com',
+  password: 'helloworld',
+  role:     'admin'
 )
 
+member = User.create!(
+  name:     'Member User',
+  email:    'member@example.com',
+  password: 'helloworld'
+)
 puts "Seed finished"
 puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
